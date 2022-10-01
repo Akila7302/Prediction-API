e2d5ba05c97a487a8c8b23ed13b9de707ce1fe15
Akila7302 <bandara98akila@gmail.com>
2022-10-01 21:55:25 +0530

    added month suggesting model

--- main.py
+++ main.py
@@ -44,6 +44,15 @@ def timesuggest(lst):
     return pred_value
 
 
+# Loading up the trained model for get the best month
+def monthsuggest(lst):
+    filename = './model/bestMonth.pkl'
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+    pred_value = model.predict([lst]).tolist()[0]
+    return pred_value
+
+
 # Defining the model input types
 class Item(BaseModel):
     id: int
@@ -110,6 +119,50 @@ async def get_predict(data: Item):
             else '8PM' if pred_value == 20
             else 'Can visit anytime' if pred_value == 24
             else 'It is not the best time to visit!',
+        }
+    }
+
+
+# Setting up the prediction route for best time
+@app.post("/monthsuggest")
+async def get_predict(data: Item):
+    feature_list = []
+
+    feature_list.append(int(data.id))
+    feature_list.append(int(data.month))
+    feature_list.append(int(data.day))
+    feature_list.append(int(data.time))
+
+    pred_value = monthsuggest(feature_list)
+
+    return {
+        "data": {
+            'prediction': pred_value,
+            'best_month': 'All months' if pred_value == 1
+            else 'May-June' if pred_value == 2
+            else 'April-September' if pred_value == 3
+            else 'November-April' if pred_value == 4
+            else 'March-May' if pred_value == 5
+            else 'December-April' if pred_value == 6
+            else 'January-September' if pred_value == 7
+            else 'December-March' if pred_value == 8
+            else 'June-July' if pred_value == 9
+            else 'July-September' if pred_value == 10
+            else 'January-March' if pred_value == 11
+            else 'November-May' if pred_value == 12
+            else 'February-September' if pred_value == 13
+            else 'September-December' if pred_value == 14
+            else 'November-March' if pred_value == 15
+            else 'March-October' if pred_value == 16
+            else 'January-April' if pred_value == 17
+            else 'January-May' if pred_value == 18
+            else 'October-January' if pred_value == 19
+            else 'February-April' if pred_value == 20
+            else 'May-September' if pred_value == 21
+            else 'February-June' if pred_value == 22
+            else 'December-January' if pred_value == 23
+            else 'Temporarily closed' if pred_value == 50
+            else '',
         }
     }
 
